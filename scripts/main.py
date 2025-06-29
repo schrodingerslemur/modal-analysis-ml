f36bd2cf10dcf1832811b2036a85618d9dc079ae
schrodinger's lemur <bhendrat@ford.com>
2025-06-29 17:47:04 -0400

    changed 'file' to 'path'

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,10 @@
 import argparse
-import sys
-import os 
 
 from core.parser.modalParser import ModalParser
 from core.analyser.modalAnalyser import ModalAnalyser
 
-def main(dat_file, inp_file):
-    model=ModalParser(dat_file, inp_file)
+def main(dat_path, inp_path):
+    model=ModalParser(dat_path, inp_path)
     analyser = ModalAnalyser(model)
     passed = analyser.check(tangential=True)
 
@@ -17,12 +15,12 @@ def main(dat_file, inp_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run modal analysis.")
-    parser.add_argument("-dat", "--dat_file", required=True, help="Path to the .dat file")
-    parser.add_argument("-inp", "--inp_file", required=False, help="Path to the .inp file (optional)")
+    parser.add_argument("-dat", "--dat_path", required=True, help="Path to the .dat file")
+    parser.add_argument("-inp", "--inp_path", required=False, help="Path to the .inp file (optional)")
     args = parser.parse_args()
 
-    dat_file = args.dat_file
-    inp_file = args.inp_file
+    dat_path = args.dat_path
+    inp_path = args.inp_path
 
-    main(dat_file, inp_file)
+    main(dat_path, inp_path)
 
