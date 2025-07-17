bbde2d0526b808ccd26abca5974c8d26a503113a
schrodinger's lemur <bhendrat@ford.com>
2025-07-17 14:10:22 -0400

    Update .gitignore and untrack ignored files

--- config.py
+++
@@ -1,4 +0,0 @@
-# HVTrans executable
-HVTRANS =  r"C:\Users\BHENDRAT\AppData\Roaming\winbin\altair\hyperworks\2025.1.0.9\vendor\hwdesktop\io\result_readers\bin\win64\hvtrans.exe"
-# Data directory (should include "inp", "dat", "odb") -> results stored in "datasets"
-DATADIR = r"C:\Users\BHENDRAT\work\modal-analysis\ml_data"
--- scripts/plot.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from core.preprocessing.filter.plot import plot_max_disp
-
-from config import DATADIR
-
-if __name__ == "__main__":
-    dat_path = Path(DATADIR) / "dat"
-    plot_max_disp(dat_path)
