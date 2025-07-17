3037c29ce5574aa7fdcd304af256a386850cb1de
schrodinger's lemur <bhendrat@ford.com>
2025-07-17 14:03:14 -0400

    added model1 and model2 preproccesing scripts

--- core/preprocessing/io/create_json.py
+++ core/preprocessing/io/create_json.py
@@ -32,22 +32,44 @@ def create_json_from_inp(inp_file, dest_file):
     return json_output
 
 # For results
-def create_json_from_dat(dat_file, dest_file):
+def create_result_json_from_dat(dat_file, dest_file): #TODO: change (one for each mode)
     assert(dat_file.endswith(".dat"), "Source file must be a .dat file")
     assert(dest_file.endswith(".json"), "Destination file must be a .json file")
 
     parser = DATParser(dat_file)
     freq = []
-    for i in range(1, 51):
+    for i in range(1, 51): # TODO: change range to max modes
         freq.append(parser.get_freq(i))
 
     data= [{
-        "label": f"Modal frequencies",
+        "label": "Modal frequencies",
         "type": "vector",
         "data": freq
     }]
 
     json_output = json.dumps(data, indent=4)
+
+    # Create and write to file
+    os.makedirs(os.path.dirname(dest_file), exist_ok=True)
+    with open(dest_file, "w") as f:
+        f.write(json_output)
+
+    return json_output
+
+# For input
+def create_input_json_from_dat(dat_file, mode, dest_file):
+    assert(dat_file.endswith(".dat"), "Source file must be a .dat file")
+    assert(dest_file.endswith(".json"), "Destination file must be a .json file")
+
+    parser = DATParser(dat_file)
+    label = "Frequency"
+    data = [parser.get_freq(mode)]
+    input_data = [{
+        "label": label,
+        "data": data
+    }]
+
+    json_output = json.dumps(input_data, indent=4)
 
     # Create and write to file
     os.makedirs(os.path.dirname(dest_file), exist_ok=True)
--- scripts/model1_preprocess.py
+++ scripts/model1_preprocess.py
@@ -1,21 +1,13 @@
-import os
-import sys
 import argparse
 from pathlib import Path
 
 from config import DATADIR
-from core.preprocessing.io.create_json import create_json_from_inp, create_json_from_dat
+from core.preprocessing.io.create_json import create_result_json_from_dat
 from core.preprocessing.io.create_h3d import create_h3d_from_odb
 
 def main(inp_dir, odb_dir, dat_dir, output_dir, config_file, range=None):
     # Create global hooks
     output_dir.mkdir(parents=True, exist_ok=True)
-
-    for inp_file in inp_dir.glob("*.inp"):
-        if range and (int(inp_file.stem) < range[0] or int(inp_file.stem) > range[1]):
-            continue
-        output_path = str(output_dir / f"{inp_file.stem}_inp.json")
-        create_json_from_inp(str(inp_file), output_path)
 
     # Convert odb to h3d 
     for odb_file in odb_dir.glob("*.odb"):
@@ -28,24 +20,21 @@ def main(inp_dir, odb_dir, dat_dir, output_dir, config_file, range=None):
     for dat_file in dat_dir.glob("*.dat"):
         if range and (int(dat_file.stem) < range[0] or int(dat_file.stem) > range[1]):
             continue
-        output_path = str(output_dir / f"{dat_file.stem}.json")
-        create_json_from_dat(str(dat_file), output_path)
+        output_path = str(output_dir / f"{dat_file.stem}.json") #TODO: change if necessary
+        create_result_json_from_dat(str(dat_file), output_path)
 
-    # copy to global hooks dir
-
-#for input.h3d, need to have input_inp.json in _hooks, global_inputs_hook.py needs to be copied too
 if __name__ == "__main__":
     data_dir = Path(DATADIR)
     inp_dir = data_dir / "inp"
     dat_dir = data_dir / "dat"
     odb_dir = data_dir / "odb"
-    dataset_dir = data_dir / "datasets"
+    model1_dataset_dir = data_dir / "model1_datasets"
 
-    config_file = str(dataset_dir / "config.cfg")
+    config_file = str(model1_dataset_dir / "config.cfg")
     parser = argparse.ArgumentParser(description="Preprocess modal analysis data.")
     parser.add_argument('--range', type=int, nargs=2, metavar=('START', 'END'), help='Range of files to process (start end)', required=False)
     args = parser.parse_args()
 
     # If user wants to process just one file, pass the same value for start and end in --range
     # Example: --range 1 1 will process only file 1
-    main(inp_dir, odb_dir, dat_dir, dataset_dir, config_file, range=args.range if args.range else None)
+    main(inp_dir, odb_dir, dat_dir, model1_dataset_dir, config_file, range=args.range if args.range else None)
